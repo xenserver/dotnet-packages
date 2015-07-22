e6a12da8a68344018caecf874b7cfda7ca1606b8
U-NT AUTHORITY\SYSTEM <SYSTEM@tocco.do.citrite.net>
2015-07-22 22:00:20 +0100

    Changed the build script to use the v120 build tools
    Signed-off-by: Mihaela Stoica <mihaela.stoica@citrix.com>

--- mk/dotnet-packages-build.sh
+++ mk/dotnet-packages-build.sh
@@ -124,13 +124,13 @@ echo "INFO:	Performing main build tasks..."
 
 run_msbuild()
 {
-  MSBuild.exe /nologo /m /verbosity:minimal /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 $*
+  MSBuild.exe /nologo /m /verbosity:minimal /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /property:PlatformToolset=v120 $*
   return $?
 }
 
 run_msbuild_nofw()
 {
-  MSBuild.exe /nologo /m /verbosity:minimal /p:Configuration=Release $*
+  MSBuild.exe /nologo /m /verbosity:minimal /p:Configuration=Release /property:PlatformToolset=v120 $*
   return $?
 }
 
